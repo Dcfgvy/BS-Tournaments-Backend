7899e25acea4a73304dc2ec4cb8a8815d699279d
Ivan <ipkusliy@gmail.com>
2024-09-06 20:00:23 +0300

    admin module HOTFIX

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { TournamentsModule } from './tournaments/tournaments.module';
 import { EventsModule } from './events/events.module';
 import { BrawlersModule } from './brawlers/brawlers.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
-  imports: [UsersModule, TournamentsModule, EventsModule, BrawlersModule],
+  imports: [EventsModule, BrawlersModule],
   controllers: [AdminController],
   providers: [AdminService]
 })
