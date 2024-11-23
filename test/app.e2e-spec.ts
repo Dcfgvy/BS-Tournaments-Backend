5c7c59b2a109b12b06be1bf4855aaf624a3f4e4b
Ivan <ipkusliy@gmail.com>
2024-11-23 16:47:15 +0300

    fixed e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,6 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close(); // Close the app after all tests
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
